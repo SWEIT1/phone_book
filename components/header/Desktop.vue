d278fec192ba1e5e39ebed45ff7f10f11ffa64e6
Luka <luka.tsaguria@ribs.studio>
2024-01-20 00:13:53 +0200

    xuinya

--- components/header/Desktop.vue
+++ components/header/Desktop.vue
@@ -6,6 +6,7 @@
     <div class="Header__navigation">
       <nuxt-link to="/">About</nuxt-link>
       <nuxt-link to="/">Contacts</nuxt-link>
+      <nuxt-link to="/">Xuinya</nuxt-link>
     </div>
   </div>
 </template>
